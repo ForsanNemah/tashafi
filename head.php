680b55a3b20c69093608ce916d1f1b634e2cc6e5
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-11-03 16:08:54 +0300

    ssss

--- head.php
+++ head.php
@@ -177,7 +177,7 @@ snaptr('init', 'b67a49ad-d1f1-445f-941f-31aea29bc8e4');
 
 snaptr('init', 'b2436d09-c13e-4280-9181-caa1960c5448');
 //snaptr('init', 'a00b3504-1613-44db-89ed-1e40e8831c77');
-snaptr('init', '77ec2e1e-c2bb-468f-8d2e-f99b7ec9983c');
+snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
 snaptr('track', 'PAGE_VIEW');
 //snaptr('track', 'PURCHASE');
 
